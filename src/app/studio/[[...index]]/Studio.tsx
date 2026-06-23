b1585df27d75ab6b6acada981e866fde51aed945
unknown <huznigarane@gmail.com>
2026-06-23 21:41:54 +0300

    fix(studio): remove high z-index wrapper to fix dropdown menus

--- src/app/studio/[[...index]]/Studio.tsx
+++ src/app/studio/[[...index]]/Studio.tsx
@@ -4,16 +4,6 @@ import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
 export function Studio() {
-  return (
-    <div
-      style={{
-        position: 'fixed',
-        inset: 0,
-        zIndex: 99999,
-      }}
-    >
-      <NextStudio config={config} />
-    </div>
-  )
+  return <NextStudio config={config} />
 }
 
